e75c2cb3aeb525be4a5f16427c24a18c253d0f91
KimSungmin <gumona@naver.com>
2026-03-19 20:24:00 +0900

    20250319

---
+++ Step05_Function.py
@@ -0,0 +1,10 @@
+def test2(arg):
+  print(f"test2 함수 호출, 전달된 인자: {arg}")
+
+test2("안녕하세요")
+test2("Hello")
+test2("こんにちは")
+
+def print_info(name: str, age: int):
+  print(f"이름: {name}, 나이: {age}")
+
